ef37926f11d9b579761aca9ef2666a757c8112f5
Robert M <belamorris@gmail.com>
2019-02-06 19:26:05 -0700

    got everything to append now just need to do readme

--- liri.js
+++ liri.js
@@ -8,6 +8,8 @@ var axios = require("axios");
 var Spotify = require('node-spotify-api');
 //require moment
 var moment = require('moment')
+//require fs
+var fs = require('fs');
 
 //spotify keys info
 var spotify = new Spotify(keys.spotify);
@@ -16,7 +18,7 @@ var spotify = new Spotify(keys.spotify);
 var nodeArgs = process.argv;
 
 //create a var for command
-var command =  process.argv[2];
+var command = process.argv[2];
 
 // Create an empty variable for holding the search title
 var title = "";
@@ -25,92 +27,216 @@ var title = "";
 // And do a little for-loop magic to handle the inclusion of "+"s
 for (var i = 3; i < nodeArgs.length; i++) {
 
-  if (i > 3 && i < nodeArgs.length) {
-    title = title + "+" + nodeArgs[i];
-  }
-  else {
-    title += nodeArgs[i];
+    if (i > 3 && i < nodeArgs.length) {
+        title = title + "+" + nodeArgs[i];
+    } else {
+        title += nodeArgs[i];
 
-  }
+    }
 }
 
 //switch case
-switch(command){
+switch (command) {
     case "concert-this":
-      if(title){
-      concertThis(title);
-    }else {
-        console.log("Sorry can't find data")
-    }
-    break;
-  
+        if (title) {
+            concertThis(title);
+        } else {
+            console.log("Sorry can't find data")
+        }
+        break;
+
     case "spotify-this-song":
-      if(title){
-        spotifySong(title);
-      } else{
-        spotifySong("The Sign by Ace of Base");
-      }
-    break;
-  
+        if (title) {
+            spotifySong(title);
+        } else {
+            spotifySong("The Sign by Ace of Base");
+        }
+        break;
+
     case "movie-this":
-      if(title){
-        omdbData(title)
-      } else{
-        omdbData("Mr. Nobody")
-      }
-    break;
-  
+        if (title) {
+            omdbData(title)
+        } else {
+            omdbData("Mr. Nobody")
+        }
+        break;
+
     case "do-what-it-says":
-      doThing();
-    break;
-    }
+        doThing();
+        break;
+}
+//the concert-this axios function still need to do the moment for the date
+function concertThis(title) {
+    // Then run a request with axios to the OMDB API with the movie specified
+    var queryUrl = "https://rest.bandsintown.com/artists/" + title + "/events?app_id=codingbootcamp";
+//function to consolelog the axios response 
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log("-------------------------");
+            //log the lineup
+            console.log(response.data[0].lineup);
+            console.log("-------------------------");
+            //log the venue name, city and date
+            console.log(response.data[0].venue.name);
+            console.log(response.data[0].venue.city);
+            console.log(response.data[0].datetime);
+            console.log("-------------------------");
+            //log the second venue name, city and date
+            console.log(response.data[1].venue.name);
+            console.log(response.data[1].venue.city);
+            console.log(response.data[1].datetime);
+            console.log("-------------------------");
+            //log the third venue name, city and date
+            console.log(response.data[2].venue.name);
+            console.log(response.data[2].venue.city);
+            console.log(response.data[2].datetime);
+            console.log("-------------------------");
 
-    function concertThis(title){
-        // Then run a request with axios to the OMDB API with the movie specified
-var queryUrl = "https://rest.bandsintown.com/artists/" + title + "/events?app_id=codingbootcamp";
+            //append info to log.txt
+            fs.appendFile("log.txt", "\n", function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", response.data[0].lineup, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", response.data[0].venue.name, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", response.data[0].venue.city, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", response.data[0].datetime, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", response.data[1].venue.name, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", response.data[1].venue.city, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", response.data[1].datetime, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", response.data[2].venue.name, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", response.data[2].venue.city, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", response.data[2].datetime, function (err) {
+                if (err) throw err;
+            });
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
 
-axios.get(queryUrl).then(
-  function(response) {
-    console.log("Release Year: " + response.data);
-  }
-);
-    }
+        })
+}
+//the movie-this axios function
+function omdbData(title) {
+    // Then run a request with axios to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
 
-    function omdbData(title){
-        // Then run a request with axios to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+    axios.get(queryUrl).then(
+        function (response) {
+            //log the movie title
+            console.log("Title: " + response.data.Title);
+            //log the release year
+            console.log("Release Year: " + response.data.Year);
+            //log the IMdB rating
+            console.log("IMdB Rating: " + response.data.imdbRating);
+            //log the rotten tomatoes rating
+            console.log("Rotten Tomatoes Rating: " + response.data.tomatoRating);
+            //log the country it was produced
+            console.log("Country: " + response.data.Country);
+            //log the language 
+            console.log("Language: " + response.data.Language);
+            //log the plot
+            console.log("Plot: " + response.data.Plot);
+            //log the actors
+            console.log("Actors: " + response.data.Actors);
 
-axios.get(queryUrl).then(
-  function(response) {
-    console.log("Release Year: " + response.data.Year);
-  }
-);
-    }
+            // append to log.txt
+            fs.appendFile("log.txt", "\n", function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", "Title: " + response.data.Title, function (err) {
+                if (err) throw err;
+            })
+            fs.appendFile("log.txt", "Release Year: " + response.data.Year, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", "IMdB Rating: " + response.data.imdbRating, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", "Rotten Tomatoes Rating: " + response.data.tomatoRating, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", "Country: " + response.data.Country, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", "Language: " + response.data.Language, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", "Plot: " + response.data.Plot, function (err) {
+                if (err) throw err;
+            });
+            fs.appendFile("log.txt", "Actors: " + response.data.Actors, function (err) {
+                if (err) throw err;
+            });
 
-    function spotifySong(title){
-        spotify.search({ type: 'track', query: title}, function(error, data){
-            if(!error){
-              for(var i = 0; i < data.tracks.items.length; i++){
-                var songData = data.tracks.items[i];
-                //artist
-                console.log("Artist: " + songData.artists[0].name);
-                //song name
-                console.log("Song: " + songData.name);
-                //spotify preview link
-                console.log("Preview URL: " + songData.preview_url);
-                //album name
-                console.log("Album: " + songData.album.name);
-                console.log("-----------------------");
-                
-              }
-            } else{
-              console.log('Error occurred.');
-            }
-          });
-    }
+        }
+    );
+}
+//spotify-this-song function
+function spotifySong(title) {
+    spotify.search({
+        type: 'track',
+        query: title
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        // Log Artist
+        console.log(data.tracks.items[0].artists[0].name);
+        // Log Song name
+        console.log(data.tracks.items[0].name);
+        // Log URL
+        console.log(data.tracks.items[0].external_urls);
+        // Log Album Name
+        console.log(data.tracks.items[0].album.name);
+        //append to log.txt
+        fs.appendFile("log.txt", "\n", function (err) {
+            if (err) throw err;
+        });
+        // append Artist
+        fs.appendFile("log.txt", data.tracks.items[0].artists[0].name, function (err) {
+            if (err) throw err;
+        });
+        // append Song name
+        fs.appendFile("log.txt", data.tracks.items[0].name, function (err) {
+            if (err) throw err;
+        });
+        // append URL
+        fs.appendFile("log.txt", data.tracks.items[0].external_urls, function (err) {
+            if (err) throw err;
+        });
+        // append Album Name
+        fs.appendFile("log.txt", data.tracks.items[0].album.name, function (err) {
+            if (err) throw err;
+        });
+
+
+    });
+}
+
+//do-what-it-says function
+function doThing() {
+    //reads random.txt file and runs the spotify-this-song function with the info in random.txt
+    fs.readFile('random.txt', "utf8", function (error, data) {
+        var txt = data.split(',');
+//call to run spotify-this-song function
+        spotifySong(txt[1]);
+    });
+}
